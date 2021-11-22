b5173ccdba6144905e3dc07b4c21b798f404552a
Kameron Ashley <kameronashley@gmail.com>
2021-11-22 11:52:42 -0800

    commit tree 8312291039c54945c10a23f3e590885ef70d34a1

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,9 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    specific_students = Student.find_by(id: params[:id])
+    render json: specific_students
+  end
+
 end
